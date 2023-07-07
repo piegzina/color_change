a0fcc7b5b5b7dc137cd12bee5399b8136cc01b45
piegzina <106431259+piegzina@users.noreply.github.com>
2023-07-07 21:39:20 +0200

    color is class of div

---
+++ script.js
@@ -0,0 +1,51 @@
+const white = document.querySelector('.white');
+const black = document.querySelector('.black');
+const yellow = document.querySelector('.yellow');
+const green = document.querySelector('.green');
+const blue = document.querySelector('.blue');
+const red = document.querySelector('.red');
+const purple = document.querySelector('.purple');
+const pink = document.querySelector('.pink');
+const orange = document.querySelector('.orange');
+const grey = document.querySelector('.grey');
+const box = document.querySelector('.box');
+
+white.addEventListener('click', () => {
+    box.style.backgroundColor = "white";
+});
+
+black.addEventListener('click', () => {
+    box.style.backgroundColor = "black";
+});
+
+yellow.addEventListener('click', () => {
+    box.style.backgroundColor = "yellow";
+});
+
+green.addEventListener('click', () => {
+    box.style.backgroundColor = "green";
+});
+
+blue.addEventListener('click', () => {
+    box.style.backgroundColor = "blue";
+});
+
+red.addEventListener('click', () => {
+    box.style.backgroundColor = "red";
+});
+
+purple.addEventListener('click', () => {
+    box.style.backgroundColor = "purple";
+});
+
+pink.addEventListener('click', () => {
+    box.style.backgroundColor = "pink";
+});
+
+orange.addEventListener('click', () => {
+    box.style.backgroundColor = "orange";
+});
+
+grey.addEventListener('click', () => {
+    box.style.backgroundColor = "grey";
+});
